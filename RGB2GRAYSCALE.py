c8eb978eefd4d8ff96118ac9b428361c72e00c3a
MKasif11 <mkasif981@gmail.com>
2024-08-08 10:59:19 +0530

    added RGB image in output

--- RGB2GRAYSCALE.py
+++ RGB2GRAYSCALE.py
@@ -2,19 +2,24 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_image(image, title):
-    plt.imshow(image, cmap='gray')
-    plt.title(title)
-    plt.axis('off')
-
 image_path = 'gojo.jpeg'
 
 bgr_image = cv2.imread(image_path)
 
-b, g, r = cv2.split(bgr_image)
+rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+
+r, g, b = cv2.split(rgb_image)
 
 grayscale = 0.299 * r + 0.587 * g + 0.114 * b
 
 grayscale_image = grayscale.astype(np.uint8)
 
-plot_image(grayscale_image, 'Grayscale')
+plt.subplot(1,2,1)
+plt.imshow(rgb_image)
+plt.title('RGB')
+plt.axis('off')
+
+plt.subplot(1,2,2)
+plt.imshow(grayscale_image, cmap='gray')
+plt.title('GRAYSCALE')
+plt.axis('off')
